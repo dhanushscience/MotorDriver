98bb88fc96f1574684189cef4a3692c78274c142
Dhanush Science <114058080+dhanushscience@users.noreply.github.com>
2023-07-17 23:14:37 +0530

    Example

---
+++ examples/Digital_Drive/Digital_Drive.ino
@@ -0,0 +1,43 @@
+/*
+*********************************************************
+   -------------MOTOR DRIVER LIBRARY-------------
+     "created by Dhanush Science to the World"
+
+    Licence type: Open-Source (MIT Licence)
+    Github:https://github.com/dhanushscience/MotorDriver.git
+Issue/recommendation contact : contactdhanush@proton.me
+**********************************************************
+ BY DEFAULT CONNECTION & BAUD RATE
+
+  CONNECTION: 
+  INA1/ M1A ---> D5
+  INA2/ M1B ---> D6
+  INB1/ M2A ---> D9
+  INB2/ M2B ---> D10
+
+  BAUD: 9600
+  **NOTE: connect to PWM pin in case of "PWM_Drive" Mode and for "Digital_Drive" you can use any Digital pin
+
+  */
+
+#include <MotorDriver.h>
+
+MotorDriver motor;
+void setup()
+{
+  motor.begin(5,6,9,10,9600); //Declare: (M1A,M1B,M2A,M2B,BaudRate)
+  Serial.println("Motor starting");
+}
+void loop()
+{
+  motor.Forward(); // (SPEED,TIME) --> speed in percentage (1-100) , Time in Second; "BY DEFAULT 100% SPEED & NO DELAY"
+  motor.Backward(); // (SPEED,TIME) --> speed in percentage (1-100) , Time in Second; "BY DEFAULT 100% SPEED & NO DELAY"
+  motor.HardRight(); // (SPEED,TIME) --> speed in percentage (1-100) , Time in Second; "BY DEFAULT 100% SPEED & NO DELAY"
+  motor.HardLeft(); // (SPEED,TIME) --> speed in percentage (1-100) , Time in Second; "BY DEFAULT 100% SPEED & NO DELAY"
+  motor.Hardstop(); // Nothing Required here
+  motor.SoftLeft(); // (SPEED,TIME) --> speed in percentage (1-100) , Time in Second; "BY DEFAULT 100% SPEED & NO DELAY"
+  motor.SoftRight(); // (SPEED,TIME) --> speed in percentage (1-100) , Time in Second; "BY DEFAULT 100% SPEED & NO DELAY"
+  motor.RotateCW(); // (TIME) Time in Seconds--> "BY DEFAULT 100% SPEED & NO DELAY"
+  motor.RotateACW(); // (TIME) Time in Seconds--> "BY DEFAULT 100% SPEED & NO DELAY"
+  motor.SoftStop(); // Nothing Required here
+}
