5c8c9e5fd5d427067b9de6b9a0a340cb95c08ede
Dhanush Science <114058080+dhanushscience@users.noreply.github.com>
2024-11-25 22:09:24 +0530

    Update MotorDriver.h

--- MotorDriver.h
+++ MotorDriver.h
@@ -2,29 +2,33 @@
 #define MOTOR_DRIVER_H
 
 #include"Arduino.h"
+#include <stdint.h>
+
+bool EN_CON;
 
 class MotorDriver
 {
     private:
 
-        int RightForward;
-        int RightBackward;
-        int LeftForward;
-        int LeftBackward;
+        uint8_t RightForward;
+        uint8_t RightBackward;
+        uint8_t LeftForward;
+        uint8_t LeftBackward;
+        uint8_t RightEnable;
+        uint8_t LeftEnable;
 
     public:
 
         MotorDriver();
 
-        void begin(int M1A, int M1B, int M2A, int M2B,int baud = 9600);
-        void Forward(int speed=100,int time = -1);
-        void Backward(int speed=100,int time = -1);
-        void HardRight(int speed=100,int time = -1);
-        void HardLeft(int speed=100,int time = -1);
+        void begin(int8_t EA = -1, int8_t EB = -1, uint8_t M1A, uint8_t M1B, uint8_t M2A, uint8_t M2B);
+        void move(uint8_t RSpeed=255, uint8_t LSpeed=255, bool RFState=LOW, bool RBState=LOW, bool LFState=LOW, bool LBState=LOW, uint8_t time = -1);
+        void Forward(uint8_t speed=100,uint8_t time = -1);
+        void Backward(uint8_t speed=100,uint8_t time = -1);
+        void HardRight(uint8_t Rspeed=100, uint8_t Lspeed=100 ,uint8_t time = -1);
+        void HardLeft(uint8_t Rspeed=100, uint8_t Lspeed=100 ,uint8_t time = -1);
         void SoftRight(int speed=100,int time = -1);
         void SoftLeft(int speed=100,int time = -1);
-        void RotateCW(int time = -1);
-        void RotateACW(int time = -1);
         void Hardstop();
         void SoftStop();
 };
